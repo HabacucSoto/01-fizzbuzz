cf60889ba4e5b77a9f562ff7b8e3eb4d15befd5d
habacuc <josehabacuc182020@gmail.com>
2022-05-01 13:11:22 -0500

    test for fizzbuzz class

---
+++ test/lib/services/FizzBuzzService.test.js
@@ -0,0 +1,13 @@
+const FizzBuzz = require( '../../../lib/services/FizzBuzzService.js' )
+
+describe( 'Unit test for class FizzBuzz', () => {
+
+    test( '1) fizzbuzz', () => {
+
+        const explorer = { name: "Explorer1", score: 15 }
+
+        expect( FizzBuzz.applyValidationInExplorer( explorer ) ).not.toBeUndefined()
+
+    } )
+
+} )
