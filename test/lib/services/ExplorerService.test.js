cc88fa801618e6bd848e3ed15afa901c67eea55f
habacuc <josehabacuc182020@gmail.com>
2022-05-01 09:47:36 -0500

    test 2 for class ExplorerService

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -3,11 +3,21 @@ const explorers = Reader.readJsonFile( 'explorers.json' )
 const ExplorerService = require( '../../../lib/services/ExplorerService.js' )
 
 describe( 'Unit test for class ExplorerService', () => {
-
+    
     test( '1) Filter by mission', () => {
+        
+        const res = ExplorerService.filterByMission( explorers, 'node' )
 
-        expect( ExplorerService.filterByMission( explorers, 'node' ) ).not.toBeUndefined()
+        expect( res ).not.toBeUndefined()
 
     } )
+
+    test( '2) Amount of explorers by mission', () => {
+
+        const res = ExplorerService.getAmountOfExplorersByMission( explorers, 'node' )
+        
+        expect( typeof res ).toBe( 'number' )
+
+    } ) 
 
 } )
